af518bd9533ae924d5b14ffa5d01788902368674
ajay bathala <bathalaajay12@outlook.com>
2026-06-14 07:38:52 +0530

    frist account created and push code into git

---
+++ storage.tf
@@ -0,0 +1,13 @@
+resource "azurerm_storage_account" "storage-1" {
+  name                     = "storage1"
+  resource_group_name      = azurerm_resource_group.rg-1.name
+  location                 = azurerm_resource_group.rg-1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}   
+
+resource "azurerm_storage_container" "container-1" {
+  name                  = "container1"
+  storage_account_name  = azurerm_storage_account.storage-1.name
+  container_access_type = "private"
+}
---
+++ vent.tf
@@ -0,0 +1,5 @@
+resource "azurerm_virtual_network" "vnet-1" {
+  name                = "vnet-1"
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg-1.name
+}
