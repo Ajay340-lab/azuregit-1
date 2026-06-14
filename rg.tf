ce360dc9ab535bbf8422a788cefe187758bbe560
ajay bathala <bathalaajay12@outlook.com>
2026-06-14 07:45:25 +0530

    v2

---
+++ rg.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "rg-1" {  
+  name     = "rg-1"
+  location = "eastus"
+}
+
+
+resource "azurerm_virtual_network" "vnet-1" {
+  name                = "vnet-1"
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg-1.name
+}
--- vent.tf
+++ vent.tf
@@ -3,3 +3,9 @@ resource "azurerm_virtual_network" "vnet-1" {
   address_space       = ["10.0.0.0/16"]
   resource_group_name = azurerm_resource_group.rg-1.name
 }
+
+resource "azurerm_subnet" "subnet-1" {
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.rg-1.name
+  virtual_network_name = azurerm_virtual_network.vnet-1.name
+  address_prefixes     = ["10.0.1.0/24"]
